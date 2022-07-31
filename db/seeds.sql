804eb6671984ecac291e19fd8e9cc6787e36c456
diegorodd <dieegoa@icloud.com>
2022-07-30 20:26:22 -0400

    adding values of the roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,13 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 
+VALUES
+    ('HR Manager', 100000, 1),
+    ('HR', 50000, 1),
+    ('Sales', 65000, 2),
+    ('Human Resources', 195000, 3),
+    ('Lawyer', 110000, 4),
+    ('Accountant', 90000, 4);
+
+    INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
