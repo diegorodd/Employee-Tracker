0eec2e673012312eb401f0a5523ad3e077ff7e84
diegorodd <dieegoa@icloud.com>
2022-08-04 23:52:18 -0400

    finished the connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,5 +3,12 @@ const connection = mysql.createConnection({
 
     host: "localhost",
     user: "root",
-    password: ""
-})
+    password: "",
+    database: "employess"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+  });
+  
+  module.exports = connection;
