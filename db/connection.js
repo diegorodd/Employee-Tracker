dd2e2391baf6ddb1c900d22a3bdcc6fe92cc1b10
diegorodd <dieegoa@icloud.com>
2022-08-06 19:19:57 -0400

    npm install

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "employess"
+    // database: "employess"
 });
 
 connection.connect(function (err) {
